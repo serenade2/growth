90e08f7de9c673dbc516114cf79ef27fec1d9d91
Samuel <serenade.124@gmail.com>
2015-03-29 11:48:19 -0400

    - Updated NGUI Version - Created Golden Sun Atlas - Added Temporary Golden Sun Sprites

--- Assets/NGUI/Examples/Scripts/Other/PlayIdleAnimations.cs
+++ Assets/NGUI/Examples/Scripts/Other/PlayIdleAnimations.cs
@@ -1,8 +1,3 @@
-//----------------------------------------------
-//            NGUI: Next-Gen UI kit
-// Copyright © 2011-2012 Tasharen Entertainment
-//----------------------------------------------
-
 using UnityEngine;
 using System.Collections.Generic;
 
